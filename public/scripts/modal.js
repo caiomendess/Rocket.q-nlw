649f772c50742e56b03e049260a3bfb74b8453ad
Caio Mendes <caiofernando.dev@gmail.com>
2021-10-05 18:44:31 -0300

    backend started

---
+++ public/scripts/modal.js
@@ -0,0 +1,12 @@
+module.exports = function Modal(){
+    function open(){
+        /**Funcionalidade de atribuir a classe active para modal */
+    }
+    function close(){
+        /**Funcionalidade de removar a classe active da modal */
+    }
+
+        return{
+            open, close
+        }
+}
