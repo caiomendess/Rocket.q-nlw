e663a0631ba2d7d9a4b16b9b35cdbcd498562a28
Caio Mendes <caiofernando.dev@gmail.com>
2021-12-09 17:48:53 -0300

    complete

--- src/db/init.js
+++ src/db/init.js
@@ -11,8 +11,9 @@ const initDb = {
 
         await db.exec(`CREATE TABLE questions (
              id INTEGER PRIMARY KEY AUTOINCREMENT,
-             titulo TEXT,
-             reader INT
+             title TEXT,
+             reader INT,
+             room INT
          )`);
 
             await db.close()
